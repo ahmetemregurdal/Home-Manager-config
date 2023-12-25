ef64e6df224caf1080b59659b58ac61108caf0fb
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2023-12-26 00:36:05 +0300

    Fixed notification position

--- graphical/notification.nix
+++ graphical/notification.nix
@@ -4,11 +4,12 @@
 		enable = true;
 		defaultTimeout = 5 * 1000;
 		font = "Fira Code 12";
-		backgroundColor = "#1d2021";
+		backgroundColor = "#282828";
 		borderColor = "#89b482";
 		borderRadius = 10;
 		borderSize = 2;
 		textColor = "#d4be98";
+		margin = "20";
 	};
 	home.packages = with pkgs; [
 		libnotify
