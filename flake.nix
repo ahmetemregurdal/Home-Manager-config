29677b1cbba3a8ced9a64196f4f2a12b565e5a09
Ahmet Emre Gürdal <gurdalahmetemre@disroot.org>
2025-11-17 13:45:55 +0300

    Added stylix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		stylix = {
+			url = "github:nix-community/stylix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = {self, nixpkgs, home-manager, ...}@inputs:
+	outputs = {self, nixpkgs, home-manager, stylix, ...}@inputs:
 	{
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
@@ -16,6 +20,7 @@
 				./hardware-configuration.nix
 				./configuration.nix
 				home-manager.nixosModules.home-manager
+				stylix.nixosModules.stylix
 				{
 					home-manager = {
 						useGlobalPkgs = true;
