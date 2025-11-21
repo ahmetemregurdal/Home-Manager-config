44606a11b1a5313bda47d373ef2b49e5eedce248
Ahmet Emre Gürdal <gurdalahmetemre@disroot.org>
2025-11-21 23:48:08 +0300

    Actually fixed qutebrowser dark mode

--- modules/user/browser/qutebrowser.nix
+++ modules/user/browser/qutebrowser.nix
@@ -17,7 +17,7 @@ in
 			enable = true;
 			settings = {
 				changelog_after_upgrade = "patch";
-				colors.webpage.preferred_color_scheme = stylix.polarity;
+				colors.webpage.preferred_color_scheme = config.stylix.polarity;
 			};
 		};
 	};
