e1eea7543d1fe386b69bd42bc1966930125c6bb1
Ahmet Emre Gürdal <gurdalahmetemre@disroot.org>
2025-11-24 13:43:36 +0300

    Added something

--- hosts/legion/hardware-configuration.nix
+++ hosts/legion/hardware-configuration.nix
@@ -27,7 +27,12 @@
       fsType = "xfs";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+		{
+			device = "/swapfile";
+			size = 32768;
+		}
+	];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
