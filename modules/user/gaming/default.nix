7224251b8838133fe2c36f3a61371dc4d6b9b7d8
Ahmet Emre Gürdal <gurdalahmetemre@disroot.org>
2025-11-30 12:02:30 +0300

    Ryujinx

--- modules/user/gaming/default.nix
+++ modules/user/gaming/default.nix
@@ -2,6 +2,8 @@
 
 let
 	cfg = config.userSettings.gaming;
+	switchProd = ./nintendo-switch-prod.keys;
+	switchTitle = ./nintendo-switch-title.keys;
 in
 {
 	options = {
@@ -20,7 +22,23 @@ in
 			winePackages = [ pkgs.proton-ge-bin ];
 			protonPackages = [ pkgs.proton-ge-bin ];
 			runners = {
+				ryujinx = {
+					package = pkgs.ryubing;
+					settings.runner = {
+						prod_keys = lib.mkForce (builtins.toString switchProd);
+						title_keys = lib.mkForce (builtins.toString switchTitle);
+					};
+				};
 			};
+		};
+
+		home.file.".config/Ryujinx/system/prod.keys" = {
+			source = switchProd;
+			enable = true;
+		};
+		home.file.".config/Ryujinx/system/title.keys" = {
+			source = switchTitle;
+			enable = true;
 		};
 	};
 }
